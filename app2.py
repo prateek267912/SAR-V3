152b6a04bae0479dc643edebc31d186b6f9969cc
corprt <143188080+corprt@users.noreply.github.com>
2023-09-13 10:11:25 +0000

    subheader

--- app2.py
+++ app2.py
@@ -667,7 +667,7 @@ with col2_up:
 
     col1,col2 = st.columns(2)
     with col1:
-        st.markdown(###Pre-Set Questionnaire)
+        st.markdown("###Pre-Set Questionnaire")
         # Create a Pandas DataFrame with your data
 
         data = {'Questions': [" What is the victim's name?","What is the suspect's name?",' List the merchant name',' How was the bank notified?',' When was the bank notified?',' What is the fraud type?',' When did the fraud occur?',' Was the disputed amount greater than 5000 USD?',' What type of cards are involved?',' Was the police report filed?']}
