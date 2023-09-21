342c17df87993402ff96279751c06da378704936
corprt <143188080+corprt@users.noreply.github.com>
2023-09-21 19:17:38 +0530

    changes

--- app2.py
+++ app2.py
@@ -855,7 +855,7 @@ with col2_up:
                     prompt_1 = f'''Perform Name Enitity Recognition to identify Merchant as accurately as possible, given the context. A merchant is a type of business or organization that accepts payments from the customer account. Give a relevant and concise response.\n\n\
                                 Question: {query}\n\
                                 Context: {context_1}\n\
-                                Response: (Give me a concise response. Do not add any Explanation,Note.)'''
+                                Response: (Give me a concise response. Do not add any extra Explanation,Note.)'''
                     response = llama_llm(llama_13b,prompt_1)
                     chat_history[query] = response
 
@@ -905,7 +905,7 @@ with col2_up:
 
                     query = "Was the disputed amount greater than 5000 usd?"
                     context_1 = docsearch.similarity_search(query, k=5)
-                    prompt_1 =  f''' You need to act as a Financial analyst to identify the disputed amount, perform a mathematical calculation to check if the disputed amount is greater than 5000 USD or not. Given the context, give a relevant and concise response.\n\n\
+                    prompt_1 =  f''' You need to act as a Financial analyst to identify the disputed amount.Given the context, give a relevant and concise response.\n\n\
                                 Question: {query}\n\
                                 Context: {context_1}\n\
                                 Response: (Provide a concise Response in a single sentence. Do not write any extra [Explanation, Note, Descricption].)'''
