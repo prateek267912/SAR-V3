094c3bbe95f9db78d46f9a3716c1fdbee67a697b
corprt <143188080+corprt@users.noreply.github.com>
2023-09-25 21:07:21 +0530

    changes

--- app2.py
+++ app2.py
@@ -807,10 +807,8 @@ with col2_up:
                     df_res = {'SAR Recommendation':response1} 
                     df_res_new = pd.DataFrame(df_res.items(),columns=['Questions','Answers'])
 
-                    res_df_gpt.combine(df_res_new)
-
-                    # res_df_gpt_new = pd.concat([res_df_gpt, df_res_new], ignore_index=True)
-                    st.write(res_df_gpt)
+                    res_df_gpt_new = pd.concat([res_df_gpt, df_res_new], ignore_index=True)
+                    st.write(res_df_gpt_new)
                      
                    
                     
