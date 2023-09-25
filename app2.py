b4ceb4422c7d6ca87b42c56a63cd7676f02db74e
corprt <143188080+corprt@users.noreply.github.com>
2023-09-25 20:36:01 +0530

    changes

--- app2.py
+++ app2.py
@@ -814,9 +814,11 @@ with col2_up:
                     response1 = usellm(prompt)   
 
                     df_res = {'SAR Recommendation':response1} 
-                    df = pd.DataFrame(df_res.items(),columns=['Questions','Answers'])
-                    df.reset_index(drop=True, inplace=True)
-                    st.write(df)
+                    df_res_new = pd.DataFrame(df_res.items(),columns=['Questions','Answers'])
+                    # st.write(df_res_new)
+
+                    res_df_gpt_new = pd.concat([ res_df_gpt,df_res_new], ignore_index=True)
+                    st.write(res_df_gpt_new)
                           
 
 
